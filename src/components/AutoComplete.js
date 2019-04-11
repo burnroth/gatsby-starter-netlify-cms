69a4ef36780f52fe8cf46a4663a691e4a8096ba8
burnroth <marcus,bernroth@lime.tech>
2019-04-11 17:07:36 +0200

    rendered the search results on the page

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,55 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Suggestions from '../components/Suggestions'
+import React, { Component } from "react";
+import Suggestions from "../components/Suggestions";
+import { Link } from "gatsby";
 
-const API_URL = 'https://api.datamuse.com/words'
+
+const API_URL = "https://gcqupcrlpd.execute-api.eu-west-1.amazonaws.com/v1/staging/search";
 
 class Search extends Component {
-	state = {
-    query: '',
+  state = {
+    query: "",
     results: []
-	}
- 
+  };
 
   getInfo = () => {
-    axios.get(`${API_URL}?sl=${this.state.query}`)
-      .then(({ data }) => {
-        this.setState({
-          results: data // MusicGraph returns an object named data, 
-                             // as does axios. So... data.data                             
-        })
-      })
-  }
+    const postQuery = this.state.query;
+    const postBody = JSON.stringify({
+      query:
+        "query findOrganizations($query: String!) { " +
+        "organizations(query: $query) { " +
+        "id name city " +
+        "}}",
+      variables: { query: postQuery }
+    });
 
+    fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: postBody
+    }).then(response => response.json()).then(orgArray => {
+        this.setState({
+          results: orgArray.data.organizations
+        });
+    });
+  };
 
   handleInputChange = () => {
-    this.setState({
-      query: this.search.value
-    }, () => {
-      if (this.state.query && this.state.query.length > 1) {
-        if (this.state.query.length % 2 === 0) {
-          this.getInfo()
+    this.setState(
+      {
+        query: this.search.value
+      },
+      () => {
+        if (this.state.query && this.state.query.length > 1) {
+          if (this.state.query.length % 2 === 0) {
+            this.getInfo();
+          }
+        } else if (!this.state.query) {
         }
-      } else if (!this.state.query) {
       }
-    })
-  }
+    );
+  };
 
-
-	render() {
-		return (
-			<form>
-				<input
-        placeholder="Search for..."
-        ref={input => this.search = input}
-        onChange={this.handleInputChange}
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => (this.search = input)}
+          onChange={this.handleInputChange}
         />
         <Suggestions results={this.state.results} />
-			</form>
-		)
-	}
+      </form>
+    );
+  }
 }
 
-export default Search
-
-
+export default Search;
